c31b0bd14c2490242881237989c005b202d031af
Raphaël Gertz <git@rapsys.eu>
2019-11-27 01:23:30 +0100

    Build context in constructor Merge it in views context Remove admin member function Add policy member function Cleanup

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -13,23 +13,45 @@ use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\NamedAddress;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Translation\TranslatorInterface;
 
 class DefaultController extends AbstractController {
 	//Config array
 	protected $config;
 
+	//Context array
+	protected $context;
+
 	//Translator instance
 	protected $translator;
 
-	public function __construct(ContainerInterface $container, TranslatorInterface $translator) {
+	/**
+	 * Inject container and translator interface
+	 */
+	public function __construct(ContainerInterface $container, TranslatorInterface $translator, RouterInterface $router) {
 		//Retrieve config
 		$this->config = $container->getParameter($this->getAlias());
 
 		//Set the translator
 		$this->translator = $translator;
+
+		//Set the context
+		$this->context = [
+			'copy_long' => $translator->trans($this->config['copy']['long']),
+			'copy_short' => $translator->trans($this->config['copy']['short']),
+			'site_ico' => $this->config['site']['ico'],
+			'site_logo' => $this->config['site']['logo'],
+			'site_png' => $this->config['site']['png'],
+			'site_svg' => $this->config['site']['svg'],
+			'site_title' => $translator->trans($this->config['site']['title']),
+			'site_url' => $router->generate('rapsys_air_index')
+		];
 	}
 
+	/**
+	 * The contact page
+	 */
 	public function contact(Request $request, MailerInterface $mailer) {
 		//Set section
 		$section = $this->translator->trans('Contact');
@@ -70,14 +92,9 @@ class DefaultController extends AbstractController {
 					//Set context
 					->context(
 						[
-							'site_logo' => $this->config['site']['logo'],
-							'site_title' => $this->config['site']['title'],
-							'site_url' => $this->get('router')->generate('rapsys_air_homepage', [], UrlGeneratorInterface::ABSOLUTE_URL),
-							'copy_long' => $this->config['copy']['long'],
-							'copy_short' => $this->config['copy']['short'],
 							'subject' => $data['subject'],
 							'message' => strip_tags($data['message']),
-						]
+						]+$this->context
 					);
 
 				//Try sending message
@@ -102,226 +119,40 @@ class DefaultController extends AbstractController {
 		}
 
 		//Render template
-		return $this->render('@RapsysAir/form/contact.html.twig', ['title' => $title, 'section' => $section, 'form' => $form->createView(), 'sent' => $request->query->get('sent', 0)]);
+		return $this->render('@RapsysAir/form/contact.html.twig', ['title' => $title, 'section' => $section, 'form' => $form->createView(), 'sent' => $request->query->get('sent', 0)]+$this->context);
 	}
 
-	public function index() {
+	/**
+	 * The index page
+	 */
+	public function index(Request $request) {
 		//Set section
 		$section = $this->translator->trans('Index');
 
 		//Set title
-		$title = $section.' - '.$this->translator->trans($this->config['site']['title']);
+		$title = $section.' - '.$this->context['site_title'];
 
 		//Render template
-		return $this->render('@RapsysAir/page/index.html.twig', ['title' => $title, 'section' => $section]);
-	}
-
-	public function admin(Request $request) {
-		//Prevent non-admin to access here
-		$this->denyAccessUnlessGranted('ROLE_GUEST', null, 'Unable to access this page without ROLE_GUEST!');
-
-		//Set section
-		$section = $this->translator->trans('Admin');
-
-		//Set title
-		$title = $section.' - '.$this->translator->trans($this->config['site']['title']);
-
-		header('Content-Type: text/plain');
-		var_dump('TODO');
-		exit;
-		//Create the form according to the FormType created previously.
-		//And give the proper parameters
-		$form = $this->createForm('Rapsys\AirBundle\Form\ApplicationType', null, [
-			// To set the action use $this->generateUrl('route_identifier')
-			'action' => $this->generateUrl('rapsys_air_admin'),
-			'method' => 'POST',
-			'attr' => [ 'class' => 'form_col' ]
-		]);
-
-		//Get doctrine
-		$doctrine = $this->getDoctrine();
-
-		//Handle request
-		if ($request->isMethod('POST')) {
-			// Refill the fields in case the form is not valid.
-			$form->handleRequest($request);
-
-			if ($form->isValid()) {
-				//Get data
-				$data = $form->getData();
-
-				//Get manager
-				$manager = $doctrine->getManager();
-
-				//Protect session fetching
-				try {
-					$session = $doctrine->getRepository(Session::class)->findOneByLocationSlotDate($data['location'], $data['slot'], $data['date']);
-				//Catch no session case
-				} catch (\Doctrine\ORM\NoResultException $e) {
-					//Create the session
-					$session = new Session();
-					$session->setLocation($data['location']);
-					$session->setSlot($data['slot']);
-					$session->setDate($data['date']);
-					$session->setCreated(new \DateTime('now'));
-					$session->setUpdated(new \DateTime('now'));
-					$manager->persist($session);
-					//Flush to get the ids
-					#$manager->flush();
-				}
-
-				//Init application
-				$application = false;
-
-				//Protect application fetching
-				try {
-					//TODO: handle admin case where we provide a user in extra
-					$application = $doctrine->getRepository(Application::class)->findOneBySessionUser($session, $this->getUser());
-
-					//Add error message to mail field
-					$form->get('slot')->addError(new FormError($this->translator->trans('Application already exists')));
-				//Catch no application cases
-				//XXX: combine these catch when php 7.1 is available
-				} catch (\Doctrine\ORM\NoResultException $e) {
-				//Catch invalid argument because session is not already persisted
-				} catch(\Doctrine\ORM\ORMInvalidArgumentException $e) {
-				}
-
-				//Create new application if none found
-				if (!$application) {
-					//Create the application
-					$application = new Application();
-					$application->setSession($session);
-					//TODO: handle admin case where we provide a user in extra
-					$application->setUser($this->getUser());
-					$application->setCreated(new \DateTime('now'));
-					$application->setUpdated(new \DateTime('now'));
-					$manager->persist($application);
-
-					//Flush to get the ids
-					$manager->flush();
-
-					//Add notice in flash message
-					$this->addFlash('notice', $this->translator->trans('Application request the %date% for %location% on the slot %slot% saved', ['%location%' => $data['location']->getTitle(), '%slot%' => $data['slot']->getTitle(), '%date%' => $data['date']->format('Y-m-d')]));
-
-					//Redirect to cleanup the form
-					return $this->redirectToRoute('rapsys_air_admin');
-				}
-			}
-		}
-
-		//Compute period
-		$period = new \DatePeriod(
-			//Start from first monday of week
-			new \DateTime('Monday this week'),
-			//Iterate on each day
-			new \DateInterval('P1D'),
-			//End with next sunday and 4 weeks
-			new \DateTime('Monday this week + 5 week')
-		);
-
-		//Fetch sessions
-		$sessions = $doctrine->getRepository(Session::class)->findByDatePeriod($period);
-
-		//Init calendar
-		$calendar = [];
-		
-		//Init month
-		$month = null;
-
-		//Iterate on each day
-		foreach($period as $date) {
-			//Init day in calendar
-			$calendar[$Ymd = $date->format('Ymd')] = [
-				'title' => $date->format('d'),
-				'class' => [],
-				'sessions' => []
-			];
-			//Append month for first day of month
-			if ($month != $date->format('m')) {
-				$month = $date->format('m');
-				$calendar[$Ymd]['title'] .= '/'.$month;
-			}
-			//Deal with today
-			if ($date->format('U') == ($today = strtotime('today'))) {
-				$calendar[$Ymd]['title'] .= '/'.$month;
-				$calendar[$Ymd]['current'] = true;
-				$calendar[$Ymd]['class'][] =  'current';
-			}
-			//Disable passed days
-			if ($date->format('U') < $today) {
-				$calendar[$Ymd]['disabled'] = true;
-				$calendar[$Ymd]['class'][] =  'disabled';
-			}
-			//Set next month days
-			if ($date->format('m') > date('m')) {
-				$calendar[$Ymd]['next'] = true;
-				$calendar[$Ymd]['class'][] =  'next';
-			}
-			//Iterate on each session to find the one of the day
-			foreach($sessions as $session) {
-				if (($sessionYmd = $session->getDate()->format('Ymd')) == $Ymd) {
-					//Count number of application
-					$count = count($session->getApplications());
-
-					//Compute classes
-					$class = [];
-					if ($session->getApplication()) {
-						$class[] = 'granted';
-					} elseif ($count == 0) {
-						$class[] = 'orphaned';
-					} elseif ($count > 1) {
-						$class[] = 'disputed';
-					} else {
-						$class[] = 'pending';
-					}
-
-					//Add the session
-					$calendar[$Ymd]['sessions'][$session->getSlot()->getId().$session->getLocation()->getId()] = [
-						'id' => $session->getId(),
-						'title' => ($count > 1?'['.$count.'] ':'').$session->getSlot()->getTitle().' '.$session->getLocation()->getTitle(),
-						'class' => $class
-					];
-				}
-			}
-
-			//Sort sessions
-			ksort($calendar[$Ymd]['sessions']);
-		}
-
-		return $this->render('@RapsysAir/admin/index.html.twig', ['title' => $title, 'section' => $section, 'form' => $form->createView(), 'calendar' => $calendar]);
-	}
-
-	public function session(Request $request, $id) {
-		/*header('Content-Type: text/plain');
-		var_dump($calendar);
-		exit;*/
-
-		//Set section
-		$section = $this->translator->trans('Session %id%', ['%id%' => $id]);
-
-		//Set title
-		$title = $section.' - '.$this->translator->trans($this->config['site']['title']);
-
-		//Create the form according to the FormType created previously.
-		//And give the proper parameters
-		/*$form = $this->createForm('Rapsys\AirBundle\Form\ApplicationType', null, [
-			// To set the action use $this->generateUrl('route_identifier')
-			'action' => $this->generateUrl('rapsys_air_admin'),
-			'method' => 'POST',
-			'attr' => [ 'class' => 'form_col' ]
-		]);*/
-
-		//Get doctrine
-		$doctrine = $this->getDoctrine();
-
-		//Fetch session
-		$session = $doctrine->getRepository(Session::class)->findOneById($id);
-
-		return $this->render('@RapsysAir/admin/session.html.twig', ['title' => $title, 'section' => $section, /*'form' => $form->createView(),*/ 'session' => $session]);
+		return $this->render('@RapsysAir/default/index.html.twig', ['title' => $title, 'section' => $section]+$this->context);
 	}
 
 	/**
+	 * The policy page
+	 */
+	public function policy(Request $request) {
+		//Set section
+		$section = $this->translator->trans('Policy');
+
+		//Set title
+		$title = $section.' - '.$this->context['site_title'];
+
+		//Render template
+		return $this->render('@RapsysAir/default/policy.html.twig', ['title' => $title, 'section' => $section]+$this->context);
+	}
+
+	/**
+	 * Return the bundle alias
+	 *
 	 * {@inheritdoc}
 	 */
 	public function getAlias() {
