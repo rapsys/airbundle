75ff1e5cd7020a5a99a3c0c514ea1b7117b4106c
Raphaël Gertz <git@rapsys.eu>
2019-12-11 05:25:57 +0100

    Cleanup

--- Entity/Vote.php
+++ Entity/Vote.php
@@ -5,137 +5,126 @@ namespace Rapsys\AirBundle\Entity;
 /**
  * Vote
  */
-class Vote
-{
-    /**
-     * @var integer
-     */
-    private $id;
+class Vote {
+	/**
+	 * @var integer
+	 */
+	private $id;
 
-    /**
-     * @var \DateTime
-     */
-    private $created;
+	/**
+	 * @var \DateTime
+	 */
+	private $created;
 
-    /**
-     * @var \DateTime
-     */
-    private $updated;
+	/**
+	 * @var \DateTime
+	 */
+	private $updated;
 
-    /**
-     * @var \Rapsys\AirBundle\Entity\Application
-     */
-    private $application;
+	/**
+	 * @var \Rapsys\AirBundle\Entity\Application
+	 */
+	private $application;
 
-    /**
-     * @var \Rapsys\AirBundle\Entity\User
-     */
-    private $user;
+	/**
+	 * @var \Rapsys\AirBundle\Entity\User
+	 */
+	private $user;
 
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId() {
+		return $this->id;
+	}
 
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * Set created
+	 *
+	 * @param \DateTime $created
+	 *
+	 * @return Vote
+	 */
+	public function setCreated($created) {
+		$this->created = $created;
 
-    /**
-     * Set created
-     *
-     * @param \DateTime $created
-     *
-     * @return Vote
-     */
-    public function setCreated($created)
-    {
-        $this->created = $created;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Get created
+	 *
+	 * @return \DateTime
+	 */
+	public function getCreated() {
+		return $this->created;
+	}
 
-    /**
-     * Get created
-     *
-     * @return \DateTime
-     */
-    public function getCreated()
-    {
-        return $this->created;
-    }
+	/**
+	 * Set updated
+	 *
+	 * @param \DateTime $updated
+	 *
+	 * @return Vote
+	 */
+	public function setUpdated($updated) {
+		$this->updated = $updated;
 
-    /**
-     * Set updated
-     *
-     * @param \DateTime $updated
-     *
-     * @return Vote
-     */
-    public function setUpdated($updated)
-    {
-        $this->updated = $updated;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Get updated
+	 *
+	 * @return \DateTime
+	 */
+	public function getUpdated() {
+		return $this->updated;
+	}
 
-    /**
-     * Get updated
-     *
-     * @return \DateTime
-     */
-    public function getUpdated()
-    {
-        return $this->updated;
-    }
+	/**
+	 * Set application
+	 *
+	 * @param \Rapsys\AirBundle\Entity\Application $application
+	 *
+	 * @return Vote
+	 */
+	public function setApplication(\Rapsys\AirBundle\Entity\Application $application = null) {
+		$this->application = $application;
 
-    /**
-     * Set application
-     *
-     * @param \Rapsys\AirBundle\Entity\Application $application
-     *
-     * @return Vote
-     */
-    public function setApplication(\Rapsys\AirBundle\Entity\Application $application = null)
-    {
-        $this->application = $application;
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Get application
+	 *
+	 * @return \Rapsys\AirBundle\Entity\Application
+	 */
+	public function getApplication() {
+		return $this->application;
+	}
 
-    /**
-     * Get application
-     *
-     * @return \Rapsys\AirBundle\Entity\Application
-     */
-    public function getApplication()
-    {
-        return $this->application;
-    }
+	/**
+	 * Set user
+	 *
+	 * @param \Rapsys\AirBundle\Entity\User $user
+	 *
+	 * @return Vote
+	 */
+	public function setUser(\Rapsys\AirBundle\Entity\User $user = null) {
+		$this->user = $user;
 
-    /**
-     * Set user
-     *
-     * @param \Rapsys\AirBundle\Entity\User $user
-     *
-     * @return Vote
-     */
-    public function setUser(\Rapsys\AirBundle\Entity\User $user = null)
-    {
-        $this->user = $user;
+		return $this;
+	}
 
-        return $this;
-    }
-
-    /**
-     * Get user
-     *
-     * @return \Rapsys\AirBundle\Entity\User
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
+	/**
+	 * Get user
+	 *
+	 * @return \Rapsys\AirBundle\Entity\User
+	 */
+	public function getUser() {
+		return $this->user;
+	}
 }
