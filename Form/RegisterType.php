004655a149ac34413e394055fb5d81fc031a723d
Raphaël Gertz <git@rapsys.eu>
2021-08-08 19:58:23 +0200

    Allow phone field to be optional

--- Form/RegisterType.php
+++ Form/RegisterType.php
@@ -5,6 +5,7 @@ namespace Rapsys\AirBundle\Form;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegisterType extends \Rapsys\UserBundle\Form\RegisterType {
@@ -12,9 +13,32 @@ class RegisterType extends \Rapsys\UserBundle\Form\RegisterType {
 	 * {@inheritdoc}
 	 */
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-		return parent::buildForm($builder, $options)
-			->add('phone', TelType::class, ['attr' => ['placeholder' => 'Your phone'], 'required' => false]);
+		//Call parent build form
+		$form = parent::buildForm($builder, $options);
+
+		//Add extra phone field
+		if (!empty($options['phone'])) {
+			$form->add('phone', TelType::class, ['attr' => ['placeholder' => 'Your phone'], 'required' => false]);
+		}
+
+		//Return form
+		return $form;
 	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function configureOptions(OptionsResolver $resolver) {
+		//Call parent configure options
+		parent::configureOptions($resolver);
+
+		//Set defaults
+		$resolver->setDefaults(['phone' => true]);
+
+		//Add extra mail option
+		$resolver->setAllowedTypes('phone', 'boolean');
+	}
+
 
 	/**
 	 * {@inheritdoc}
