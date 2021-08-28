15a735d7556a74b145b8c1aa3dbf8e93e9662131
Raphaël Gertz <git@rapsys.eu>
2021-08-28 09:57:23 +0200

    Remove donate, link and profile Readd pseudonym and slug Cleanup

--- Form/RegisterType.php
+++ Form/RegisterType.php
@@ -13,7 +13,6 @@ namespace Rapsys\AirBundle\Form;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -26,24 +25,19 @@ class RegisterType extends \Rapsys\UserBundle\Form\RegisterType {
 		//Call parent build form
 		$form = parent::buildForm($builder, $options);
 
-		//Add extra donate field
-		if (!empty($options['donate'])) {
-			$form->add('donate', UrlType::class, ['attr' => ['placeholder' => 'Your donate'], 'required' => false]);
-		}
-
-		//Add extra link field
-		if (!empty($options['link'])) {
-			$form->add('link', UrlType::class, ['attr' => ['placeholder' => 'Your link'], 'required' => false]);
-		}
-
 		//Add extra phone field
 		if (!empty($options['phone'])) {
 			$form->add('phone', TelType::class, ['attr' => ['placeholder' => 'Your phone'], 'required' => false]);
 		}
 
-		//Add extra profile field
-		if (!empty($options['profile'])) {
-			$form->add('profile', UrlType::class, ['attr' => ['placeholder' => 'Your profile'], 'required' => false]);
+		//Add extra pseudonym field
+		if (!empty($options['pseudonym'])) {
+			$form->add('pseudonym', TextType::class, ['attr' => ['placeholder' => 'Your pseudonym'], 'constraints' => [new NotBlank(['message' => 'Please provide your pseudonym'])]]);
+		}
+
+		//Add extra slug field
+		if (!empty($options['slug'])) {
+			$form->add('slug', TextType::class, ['attr' => ['placeholder' => 'Your slug'], 'required' => false]);
 		}
 
 		//Return form
@@ -58,19 +52,16 @@ class RegisterType extends \Rapsys\UserBundle\Form\RegisterType {
 		parent::configureOptions($resolver);
 
 		//Set defaults
-		$resolver->setDefaults(['donate' => true, 'link' => true, 'phone' => true, 'profile' => true]);
-
-		//Add extra donate option
-		$resolver->setAllowedTypes('donate', 'boolean');
-
-		//Add extra link option
-		$resolver->setAllowedTypes('link', 'boolean');
+		$resolver->setDefaults(['phone' => true, 'pseudonym' => true, 'slug' => true]);
 
 		//Add extra phone option
 		$resolver->setAllowedTypes('phone', 'boolean');
 
-		//Add extra profile option
-		$resolver->setAllowedTypes('profile', 'boolean');
+		//Add extra pseudonym option
+		$resolver->setAllowedTypes('pseudonym', 'boolean');
+
+		//Add extra slug option
+		$resolver->setAllowedTypes('slug', 'boolean');
 	}
 
 
