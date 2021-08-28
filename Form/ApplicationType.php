c7d7cb5720cec3759e68a758f5246e439ff6eb6f
Raphaël Gertz <git@rapsys.eu>
2021-08-28 11:13:06 +0200

    Cleanup

--- Form/ApplicationType.php
+++ Form/ApplicationType.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
 /*
- * This file is part of the Rapsys PackBundle package.
+ * This file is part of the Rapsys AirBundle package.
  *
  * (c) Raphaël Gertz <symfony@rapsys.eu>
  *
