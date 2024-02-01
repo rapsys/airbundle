a930f18cade72c3bb31800c09d2126d7f7770a04
Raphaël Gertz <git@rapsys.eu>
2024-02-01 04:21:05 +0100

    Fix configuration tree

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -11,21 +11,29 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
  */
 class Configuration implements ConfigurationInterface {
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfigTreeBuilder() {
-        $treeBuilder = new TreeBuilder('rapsys_air');
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getConfigTreeBuilder() {
+		$treeBuilder = new TreeBuilder('rapsys_air');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+		// Here you should define the parameters that are allowed to
+		// configure your bundle. See the documentation linked above for
+		// more information on that topic.
 		//Set defaults
 		$defaults = [
-			'logo' => 'bundles/rapsysair/png/logo.png',
-			'title' => 'Open Air',
-			'contact_name' => 'Raphaël Gertz',
-			'contact_mail' => 'airlibre@rapsys.eu',
+			'site' => [
+				'logo' => '@RapsysAir/../public/png/logo.png',
+				'title' => 'Air Libre'
+			],
+			'copy' => [
+				'long' => 'John Doe all rights reserved',
+				'short' => 'Copyright 2019'
+			],
+			'contact' => [
+				'name' => 'John Doe',
+				'mail' => 'contact@example.com'
+			]
 		];
 
 		//Here we define the parameters that are allowed to configure the bundle.
@@ -39,13 +47,30 @@ class Configuration implements ConfigurationInterface {
 			->getRootNode()
 				->addDefaultsIfNotSet()
 				->children()
-					->scalarNode('logo')->defaultValue($defaults['logo'])->treatNullLike($defaults['logo'])->cannotBeEmpty()->end()
-					->scalarNode('title')->defaultValue($defaults['title'])->treatNullLike($defaults['title'])->cannotBeEmpty()->end()
-					->scalarNode('contact_name')->defaultValue($defaults['contact_name'])->treatNullLike($defaults['contact_name'])->cannotBeEmpty()->end()
-					->scalarNode('contact_mail')->defaultValue($defaults['contact_mail'])->treatNullLike($defaults['contact_mail'])->cannotBeEmpty()->end()
+					->arrayNode('site')
+						->addDefaultsIfNotSet()
+						->children()
+							->scalarNode('logo')->cannotBeEmpty()->defaultValue($defaults['site']['logo'])->end()
+							->scalarNode('title')->cannotBeEmpty()->defaultValue($defaults['site']['title'])->end()
+						->end()
+					->end()
+					->arrayNode('copy')
+						->addDefaultsIfNotSet()
+						->children()
+							->scalarNode('long')->defaultValue($defaults['copy']['long'])->end()
+							->scalarNode('short')->defaultValue($defaults['copy']['short'])->end()
+						->end()
+					->end()
+					->arrayNode('contact')
+						->addDefaultsIfNotSet()
+						->children()
+							->scalarNode('name')->cannotBeEmpty()->defaultValue($defaults['contact']['name'])->end()
+							->scalarNode('mail')->cannotBeEmpty()->defaultValue($defaults['contact']['mail'])->end()
+						->end()
+					->end()
 				->end()
 			->end();
 
-        return $treeBuilder;
-    }
+		return $treeBuilder;
+	}
 }
