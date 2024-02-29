dd5b71d2a6197429319bd10ae9e24913c4395ed6
Raphaël Gertz <git@rapsys.eu>
2024-02-29 18:04:47 +0100

    Cleanup

--- Form/RegisterType.php
+++ Form/RegisterType.php
@@ -14,8 +14,8 @@ namespace Rapsys\AirBundle\Form;
 use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
